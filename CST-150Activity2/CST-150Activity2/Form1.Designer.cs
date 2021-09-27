0655db30a7371248316f52d2143da5b9facddedc
AntowanG-tech <76835658+AntowanG-tech@users.noreply.github.com>
2021-09-27 00:11:37 -0700

    Activity2 submission

---
+++ CST-150Activity2/CST-150Activity2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace CST_150Activity2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fahrenheitValueInput_txtbox = new System.Windows.Forms.TextBox();
+            this.celsiusValue_txtbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.conversionAction_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fahrenheitValueInput_txtbox
+            // 
+            this.fahrenheitValueInput_txtbox.Location = new System.Drawing.Point(244, 96);
+            this.fahrenheitValueInput_txtbox.Name = "fahrenheitValueInput_txtbox";
+            this.fahrenheitValueInput_txtbox.Size = new System.Drawing.Size(100, 20);
+            this.fahrenheitValueInput_txtbox.TabIndex = 0;
+            // 
+            // celsiusValue_txtbox
+            // 
+            this.celsiusValue_txtbox.Location = new System.Drawing.Point(244, 192);
+            this.celsiusValue_txtbox.Name = "celsiusValue_txtbox";
+            this.celsiusValue_txtbox.Size = new System.Drawing.Size(100, 20);
+            this.celsiusValue_txtbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Fahrenheit Temperature";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Conversion to Celsius Temperature";
+            // 
+            // conversionAction_btn
+            // 
+            this.conversionAction_btn.Location = new System.Drawing.Point(170, 238);
+            this.conversionAction_btn.Name = "conversionAction_btn";
+            this.conversionAction_btn.Size = new System.Drawing.Size(102, 35);
+            this.conversionAction_btn.TabIndex = 4;
+            this.conversionAction_btn.Text = "Convert";
+            this.conversionAction_btn.UseVisualStyleBackColor = true;
+            this.conversionAction_btn.Click += new System.EventHandler(this.conversionAction_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 302);
+            this.Controls.Add(this.conversionAction_btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.celsiusValue_txtbox);
+            this.Controls.Add(this.fahrenheitValueInput_txtbox);
+            this.Name = "Form1";
+            this.Text = "Temperature Conversion";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fahrenheitValueInput_txtbox;
+        private System.Windows.Forms.TextBox celsiusValue_txtbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button conversionAction_btn;
+    }
+}
+
---
+++ CST-150Activity2/CST-150Activity2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150Activity2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void conversionAction_btn_Click(object sender, EventArgs e)
+        {
+            //Gets the value of the input from the textbox
+            string fahrenheitInput = fahrenheitValueInput_txtbox.Text.ToString();
+
+            
+
+            /*
+             * Mathematical conversion of the textbox input value from a Fahrenheit
+             * temperature to Celsius temperature
+             */
+            double celsiusResult = (5.0 / 9) * (double.Parse(fahrenheitInput) - 32);
+
+            //set new value to the celsius result textbox
+            celsiusValue_txtbox.Text = celsiusResult.ToString();
+            
+            MessageBox.Show("Your conversion is complete!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
