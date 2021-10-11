25f6aa8882a19576d7e8fd2129e8a9dac5cdcbbf
AntowanG-tech <antowang93@gmail.com>
2021-10-10 22:23:46 -0700

    Actvity 5 commit

---
+++ CST-150Activity5/CST-150Activity5/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace CST_150Activity5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button openTheFile_btn_Click;
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.listOfWords_txtbox = new System.Windows.Forms.TextBox();
+            this.convertedTextBox_lbl = new System.Windows.Forms.Label();
+            this.lowercase_btn_Click = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.findFirstAndLast_btn = new System.Windows.Forms.Button();
+            this.findLongestWord_btn = new System.Windows.Forms.Button();
+            this.mostVowels_btn = new System.Windows.Forms.Button();
+            this.result_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveToFile_btn = new System.Windows.Forms.Button();
+            openTheFile_btn_Click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openTheFile_btn_Click
+            // 
+            openTheFile_btn_Click.Location = new System.Drawing.Point(113, 141);
+            openTheFile_btn_Click.Name = "openTheFile_btn_Click";
+            openTheFile_btn_Click.Size = new System.Drawing.Size(155, 39);
+            openTheFile_btn_Click.TabIndex = 2;
+            openTheFile_btn_Click.Text = "Open a text file";
+            openTheFile_btn_Click.UseVisualStyleBackColor = true;
+            openTheFile_btn_Click.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            this.openFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // listOfWords_txtbox
+            // 
+            this.listOfWords_txtbox.Location = new System.Drawing.Point(332, 141);
+            this.listOfWords_txtbox.Multiline = true;
+            this.listOfWords_txtbox.Name = "listOfWords_txtbox";
+            this.listOfWords_txtbox.Size = new System.Drawing.Size(283, 367);
+            this.listOfWords_txtbox.TabIndex = 0;
+            this.listOfWords_txtbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // convertedTextBox_lbl
+            // 
+            this.convertedTextBox_lbl.AutoSize = true;
+            this.convertedTextBox_lbl.Location = new System.Drawing.Point(432, 108);
+            this.convertedTextBox_lbl.Name = "convertedTextBox_lbl";
+            this.convertedTextBox_lbl.Size = new System.Drawing.Size(61, 17);
+            this.convertedTextBox_lbl.TabIndex = 1;
+            this.convertedTextBox_lbl.Text = "File Text";
+            // 
+            // lowercase_btn_Click
+            // 
+            this.lowercase_btn_Click.Location = new System.Drawing.Point(113, 196);
+            this.lowercase_btn_Click.Name = "lowercase_btn_Click";
+            this.lowercase_btn_Click.Size = new System.Drawing.Size(155, 39);
+            this.lowercase_btn_Click.TabIndex = 3;
+            this.lowercase_btn_Click.Text = "Convert to lowercase";
+            this.lowercase_btn_Click.UseVisualStyleBackColor = true;
+            this.lowercase_btn_Click.Click += new System.EventHandler(this.lowercase_btn_Click_Click);
+            // 
+            // findFirstAndLast_btn
+            // 
+            this.findFirstAndLast_btn.Location = new System.Drawing.Point(113, 252);
+            this.findFirstAndLast_btn.Name = "findFirstAndLast_btn";
+            this.findFirstAndLast_btn.Size = new System.Drawing.Size(155, 46);
+            this.findFirstAndLast_btn.TabIndex = 4;
+            this.findFirstAndLast_btn.Text = "Find first and last word";
+            this.findFirstAndLast_btn.UseVisualStyleBackColor = true;
+            this.findFirstAndLast_btn.Click += new System.EventHandler(this.findFirstAndLast_btn_Click);
+            // 
+            // findLongestWord_btn
+            // 
+            this.findLongestWord_btn.Location = new System.Drawing.Point(113, 317);
+            this.findLongestWord_btn.Name = "findLongestWord_btn";
+            this.findLongestWord_btn.Size = new System.Drawing.Size(155, 48);
+            this.findLongestWord_btn.TabIndex = 5;
+            this.findLongestWord_btn.Text = "Find longest word";
+            this.findLongestWord_btn.UseVisualStyleBackColor = true;
+            this.findLongestWord_btn.Click += new System.EventHandler(this.findLongestWord_btn_Click);
+            // 
+            // mostVowels_btn
+            // 
+            this.mostVowels_btn.Location = new System.Drawing.Point(113, 383);
+            this.mostVowels_btn.Name = "mostVowels_btn";
+            this.mostVowels_btn.Size = new System.Drawing.Size(155, 49);
+            this.mostVowels_btn.TabIndex = 6;
+            this.mostVowels_btn.Text = "Find most vowels";
+            this.mostVowels_btn.UseVisualStyleBackColor = true;
+            this.mostVowels_btn.Click += new System.EventHandler(this.mostVowels_btn_Click);
+            // 
+            // result_textBox
+            // 
+            this.result_textBox.Location = new System.Drawing.Point(658, 141);
+            this.result_textBox.Multiline = true;
+            this.result_textBox.Name = "result_textBox";
+            this.result_textBox.Size = new System.Drawing.Size(241, 367);
+            this.result_textBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(724, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Button Stat Results";
+            // 
+            // saveToFile_btn
+            // 
+            this.saveToFile_btn.Location = new System.Drawing.Point(113, 459);
+            this.saveToFile_btn.Name = "saveToFile_btn";
+            this.saveToFile_btn.Size = new System.Drawing.Size(164, 49);
+            this.saveToFile_btn.TabIndex = 9;
+            this.saveToFile_btn.Text = "Save";
+            this.saveToFile_btn.UseVisualStyleBackColor = true;
+            this.saveToFile_btn.Click += new System.EventHandler(this.saveToFile_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 555);
+            this.Controls.Add(this.saveToFile_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result_textBox);
+            this.Controls.Add(this.mostVowels_btn);
+            this.Controls.Add(this.findLongestWord_btn);
+            this.Controls.Add(this.findFirstAndLast_btn);
+            this.Controls.Add(this.lowercase_btn_Click);
+            this.Controls.Add(openTheFile_btn_Click);
+            this.Controls.Add(this.convertedTextBox_lbl);
+            this.Controls.Add(this.listOfWords_txtbox);
+            this.Name = "Form1";
+            this.Text = "Activity 5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.TextBox listOfWords_txtbox;
+        private System.Windows.Forms.Label convertedTextBox_lbl;
+        private System.Windows.Forms.Button lowercase_btn_Click;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Button findFirstAndLast_btn;
+        private System.Windows.Forms.Button findLongestWord_btn;
+        private System.Windows.Forms.Button mostVowels_btn;
+        private System.Windows.Forms.TextBox result_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveToFile_btn;
+    }
+}
+
---
+++ CST-150Activity5/CST-150Activity5/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CST_150Activity5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+         
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                StreamReader inputFile;
+                string wordFromFile;
+                
+                
+
+                //Checks and displays whether the user chose the open or cancel buttons
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+
+                    MessageBox.Show("You clicked the Open button.");
+
+                    //Open the selected file and get a StreamReader object.
+                    inputFile = File.OpenText(openFile.FileName);
+
+                    //Clear anything currently in the listOfWords_txtbox
+                    listOfWords_txtbox.Clear();
+
+                    //Continue processing the file...
+
+
+                    //Read the file's contents.
+                    while (!inputFile.EndOfStream)
+                    {
+                        //Get a word from the List in the text file
+                        wordFromFile = inputFile.ReadToEnd();
+
+                        //add the word from the text file to the listOfWords_txtbox
+
+                        listOfWords_txtbox.Text = wordFromFile;
+
+                    }
+
+                    //close the file
+                    inputFile.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("You clicked the Cancel button.");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Display an error message
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lowercase_btn_Click_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //converts all text within the file to lower case strings
+                string toLowerCaseOutput = listOfWords_txtbox.Text.ToLower();
+                result_textBox.AppendText(Environment.NewLine);
+                result_textBox.AppendText("Lowercase Output: ");
+                result_textBox.AppendText(Environment.NewLine);
+                result_textBox.AppendText(toLowerCaseOutput);
+                result_textBox.AppendText(Environment.NewLine);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+                    }
+
+        private void findFirstAndLast_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // divides words in the document into indidvidual words
+                string wordsFromTheTextDoc = listOfWords_txtbox.Text;
+                string[] docWordArray = wordsFromTheTextDoc.Split(' ', '.', ',', '(', ')', '-', '!', '\r', '\n');
+
+                //gets the first word in alphabetical order
+                Array.Sort(docWordArray);
+
+                //remove empty values from the array
+                docWordArray = docWordArray.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                //identify the first and last word of the sorted array
+                string firstWord = docWordArray[0];
+                string lastWord = docWordArray[docWordArray.Length - 1];
+
+                result_textBox.AppendText(Environment.NewLine);
+                result_textBox.AppendText("First Word: " + firstWord);
+                result_textBox.AppendText(Environment.NewLine);
+                result_textBox.AppendText("Last Word: " + lastWord);
+                result_textBox.AppendText(Environment.NewLine);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void findLongestWord_btn_Click(object sender, EventArgs e)
+        {
+            // scan each word in the array to see if it's the longest word.
+
+            int letterCount = 0;
+            string longestWord = "";
+
+            //the text that is already displayed in the text box.
+            string wordsFromFile = listOfWords_txtbox.Text;
+
+            string[] docWordArray = wordsFromFile.Split(' ', '.', ',', '(', ')', '-', '!', '\r', '\n');
+
+            //gets the first word in alphabetical order
+            Array.Sort(docWordArray);
+
+            //remove empty values from the array
+            docWordArray = docWordArray.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            //scanning for longest word
+            foreach (string word in docWordArray)
+            {
+                if (word.Length > letterCount)
+                {
+                    longestWord = word;
+                    letterCount = word.Length;
+                }
+            }
+
+            result_textBox.AppendText(Environment.NewLine);
+            result_textBox.AppendText("Longest Word: " + longestWord);
+            result_textBox.AppendText(Environment.NewLine);
+
+
+        }
+
+        private void mostVowels_btn_Click(object sender, EventArgs e)
+        {
+            //Button for finding the word with the most vowels
+
+            //variables
+            int vowelCount = -1;
+            string wordWithMostVowels = "";
+
+            //the text that is already displayed in the text box.
+            string wordsFromFile = listOfWords_txtbox.Text;
+
+            string[] docWordArray = wordsFromFile.Split(' ', '.', ',', '(', ')', '-', '!', '\r', '\n');
+
+            //gets the first word in alphabetical order
+            Array.Sort(docWordArray);
+
+            //remove empty values from the array
+            docWordArray = docWordArray.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            //scanning for word with most vowels
+            foreach (string word in docWordArray)
+            {
+                int numberOfVowelsSoFar = 0;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    char letter = word[i];
+                    if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') numberOfVowelsSoFar++;
+                }
+                if (numberOfVowelsSoFar > vowelCount )
+                {
+                    vowelCount = numberOfVowelsSoFar;
+                    wordWithMostVowels = word;
+                }
+            }
+
+            result_textBox.AppendText(Environment.NewLine);
+            result_textBox.AppendText("Most Vowels: " + wordWithMostVowels);
+            result_textBox.AppendText(Environment.NewLine);
+
+        }
+
+        private void saveToFile_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            
+            StreamWriter outputFile;
+
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    //Open the file for appending,
+                    //and get a StreamWriter object.
+                    outputFile = File.AppendText(saveFile.FileName);
+
+                    //Write data to the file
+                    outputFile.Write(result_textBox.Text);
+
+                    //Close the file.
+                    outputFile.Close();
+
+                    //Let the user know the data was written to the file
+                    MessageBox.Show("The data has been written and saved.");
+
+                    //Clear both text box controls
+                    listOfWords_txtbox.Clear();
+                    result_textBox.Clear();
+                }
+                else
+                {
+                   MessageBox.Show("You clicked the cancel button.");
+                }
+            }
+             catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
