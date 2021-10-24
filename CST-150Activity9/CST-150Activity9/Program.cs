4dce504bfffcbcc06c83453587f66277cb12514c
AntowanG-tech <antowang93@gmail.com>
2021-10-24 16:09:31 -0700

    Acitivity 9 commit

---
+++ CST-150Activity9/CST-150Activity9/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST_150Activity9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //Method #1 Sum of two integers
+        public int sumOfTwoNumbers(int num1, int num2)
+        {
+            return 0;
+        }
+
+        //Method #2 Returns the average of five double values
+        public double averageOfFiveDoubles(double num1, double num2, double num3, double num4, double num5)
+        {
+            return 0;
+        }
+
+        //Method #3 Sum of Two Random int values
+        public int sumOfTwoRandomInts(int randomNum1, int randomNum2)
+        {
+            return 0;
+        }
+
+        //Method #4 Dis play true or false if given 3 int values and the sum thereof is divisible by 3.
+        public bool SumDivisibleByThree (int num1, int num2, int num3)
+        {
+            return false;
+        }
+
+        //Method #5  Compares two strings and displays the one that possesses fewer characters
+        public string findStringWithFewerCharacters(string string1, string string2)
+        {
+            return null;
+        }
+
+        //Method #6 Returns largest value in an array of doubles
+        public double largestValueInDoubleArray(double [] doubleArray)
+        {
+            return 0;
+        }
+
+        //Method #7 Generates and returns array that has fifty integer values
+        public int [] generateIntArray()
+        {
+            int[] intArray = null;
+
+            return intArray;
+        }
+
+        //Method #8 Write a method that takes two bool variables and returns true if they have the same value, false otherwise.
+        public bool matchingBoolValue (bool bool1, bool bool2)
+        {
+            return false;
+        }
+
+        //Method #9 Returns the result of multiplying an int and a double
+        public double multiplyIntAndDouble (int num1, double num2)
+        {
+            return 0;
+        }
+
+        // Method #10 Write a method that takes a two-dimensional array of integers and returns the average of the entries.
+        public double averageOf2DArray (int [,] twoDIntArray)
+        {
+            return 0;
+        }
+    }
+}
