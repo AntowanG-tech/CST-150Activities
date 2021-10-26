1bf4e42213f07e7a836c6aa7fbff80ffd546f58a
AntowanG-tech <antowang93@gmail.com>
2021-10-25 20:08:14 -0700

    Activity 10 commit

---
+++ CST-150Activity10/CST-150Activity10/Program.cs
@@ -0,0 +1,39 @@
+﻿//Antowan's Code
+//use for CST-150 Activity 10
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST_150Activity10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(A,B));      //changed the method call to fit two parameters instead of one.
+
+            //display orginal sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+        }
+    }
+}
---
+++ CST-150Activity10/CST-150Activity10/Set.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Antowan's code - find the errors!
+namespace CST_150Activity10
+{
+    class Set
+    {
+        private List<int> elements;
+
+        public Set()
+        {
+            elements = new List<int>();
+            
+
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                elements.Sort();                 //sorts the elements of a Set to make them easier for the user to read.
+                return true;
+            }
+
+            
+        }
+        /* I was getting a warning on line 40 that was saying the code was "unreachable." 
+         * Honestly, unless I set the breakpoint on a method in the Program.cs, nothing would populate in the console window.
+         * I ended up going line by line and commenting out the statemens until something happened. Turns out the "else return false;" statement
+         * stopped the loop from processing further as it would break the loop. 
+         * Once that line was commented out, the loop continued pass the first element
+         * 
+         * While the Set class runs the if statement, if the element does not match, it will exit the if and loop again. It will do that for the 
+         * entire Set until the loop finds a matching val. If no val(s) match, then the method will return false and there are no values that match.
+         */
+        private bool containsElement(int val)
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                    return true;
+                //else
+                   // return false;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        public Set union(Set setA, Set setB)             //modified to take parameters for both sets. The intention is to combine the two.
+        {
+            Set unifiedSet = new Set();                  //creates a new List that will hold the values of both sets with no duplicates
+
+            for (int i = 0; i < setA.elements.Count; i++)    //loop adds elements from Set A to the unifiedSet
+            {
+                unifiedSet.addElement(setA.elements[i]);
+                
+            }
+
+            for (int j = 0; j < setB.elements.Count; j++)   //loop adds elements from Set B to the unifiedSet
+            {
+                unifiedSet.addElement(setB.elements[j]);
+            }
+
+            unifiedSet.elements.Sort();                 //Sorts the elements in the unifiedSet for easier readability
+
+            return unifiedSet;
+        }
+    }
+}
