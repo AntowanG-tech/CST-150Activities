b77de675a2bb6c5fade9cd98cb749a921885e12b
AntowanG-tech <antowang93@gmail.com>
2021-10-25 23:53:27 -0700

    Activity 11 commit

---
+++ CST-150Activity11/CST-150Activity11/Dice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST_150Activity11
+{
+    class Dice
+    {
+        private int numberOfSides;
+        private int valueOfDiceFace;
+
+        public Dice()
+        {
+            numberOfSides = 6;
+        }
+
+        //This is the Dice object constructor that takes an integer between 4  and 20, inclusive and sets the number of sides of the die.
+        public Dice(int faces)
+        {
+            if (faces >= 4 && faces <= 20)          //between 4 and 20 inclusive
+            {
+                this.numberOfSides = faces;         //sets number of sides
+            }
+            else
+            {
+                Console.WriteLine("Cant roll a " + faces + "-sided die." + "Here, we'll provide you with a 6-sided die.");  //If the user tries to use a number outside of the 4 to 20 range
+                this.numberOfSides = 6;                                                                                      //they will be provided with a 6-sided die instead.
+            }
+        }
+
+        //This method will perform the action of rolling the die using the int data type
+        public int rollTheDice(Dice dice)
+        {
+            Random random = new Random();               //Here we call the random class in order to simulate the roll and randomness thereof
+
+            dice.valueOfDiceFace = random.Next(1, dice.numberOfSides + 1);      //we set the value of the user's die to the random value while rolling
+
+            return dice.valueOfDiceFace;                                //method returns the int face value of the die roll
+        }
+    }
+}
+
---
+++ CST-150Activity11/CST-150Activity11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST_150Activity11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice1 = new Dice(20);
+            Dice dice2 = new Dice(20);
+
+            rollForInitiative(dice1, dice2);
+        }
+
+        private static void rollForInitiative(Dice dice1, Dice dice2)
+        {
+            int rolls = 1;              //counts rolls made
+            bool isSakeEyes = false;      //checks for double 1's rolled
+
+            while (isSakeEyes == false)
+            {
+                int dieFaceValue1, dieFaceValue2;
+
+                dieFaceValue1 = dice1.rollTheDice(dice1);
+                dieFaceValue2 = dice2.rollTheDice(dice2);
+
+                Console.WriteLine("Roll #" + rolls + ": " + dieFaceValue1 + ", " + dieFaceValue2);
+
+                if (dieFaceValue1 == 1 && dieFaceValue2 == 1)
+                {
+                    isSakeEyes = true;
+                    Console.WriteLine("Snake Eyes!! " + "And it only took " + rolls + " to get there.");
+                    Console.WriteLine("Both adventurers roll nat 1's. They trip and fall into their own bags of holding! The End.");
+                }
+
+                rolls++;
+            }
+        }
+    }
+}
