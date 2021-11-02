62a891eb48f3831bc1a4c459a062422af0b0abda
AntowanG-tech <antowang93@gmail.com>
2021-11-02 00:23:58 -0700

    Activity 13 commit

---
+++ CST-150Activity13/CST-150Activity13/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace CST_150Activity13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topLeft_lbl = new System.Windows.Forms.Label();
+            this.topCenter_lbl = new System.Windows.Forms.Label();
+            this.topRight_lbl = new System.Windows.Forms.Label();
+            this.centerLeft_lbl = new System.Windows.Forms.Label();
+            this.center_lbl = new System.Windows.Forms.Label();
+            this.centerRight_lbl = new System.Windows.Forms.Label();
+            this.bottomLeft_lbl = new System.Windows.Forms.Label();
+            this.bottomCenter_lbl = new System.Windows.Forms.Label();
+            this.bottomRight_lbl = new System.Windows.Forms.Label();
+            this.declareWinner_lbl = new System.Windows.Forms.Label();
+            this.newGame_btn = new System.Windows.Forms.Button();
+            this.exitGame_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // topLeft_lbl
+            // 
+            this.topLeft_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.topLeft_lbl.Location = new System.Drawing.Point(49, 26);
+            this.topLeft_lbl.Name = "topLeft_lbl";
+            this.topLeft_lbl.Size = new System.Drawing.Size(167, 124);
+            this.topLeft_lbl.TabIndex = 0;
+            this.topLeft_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.topLeft_lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // topCenter_lbl
+            // 
+            this.topCenter_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.topCenter_lbl.Location = new System.Drawing.Point(249, 26);
+            this.topCenter_lbl.Name = "topCenter_lbl";
+            this.topCenter_lbl.Size = new System.Drawing.Size(172, 124);
+            this.topCenter_lbl.TabIndex = 1;
+            this.topCenter_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.topCenter_lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // topRight_lbl
+            // 
+            this.topRight_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.topRight_lbl.Location = new System.Drawing.Point(456, 26);
+            this.topRight_lbl.Name = "topRight_lbl";
+            this.topRight_lbl.Size = new System.Drawing.Size(164, 124);
+            this.topRight_lbl.TabIndex = 2;
+            this.topRight_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.topRight_lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // centerLeft_lbl
+            // 
+            this.centerLeft_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerLeft_lbl.Location = new System.Drawing.Point(49, 178);
+            this.centerLeft_lbl.Name = "centerLeft_lbl";
+            this.centerLeft_lbl.Size = new System.Drawing.Size(167, 113);
+            this.centerLeft_lbl.TabIndex = 3;
+            this.centerLeft_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.centerLeft_lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // center_lbl
+            // 
+            this.center_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.center_lbl.Location = new System.Drawing.Point(249, 178);
+            this.center_lbl.Name = "center_lbl";
+            this.center_lbl.Size = new System.Drawing.Size(172, 113);
+            this.center_lbl.TabIndex = 4;
+            this.center_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.center_lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // centerRight_lbl
+            // 
+            this.centerRight_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerRight_lbl.Location = new System.Drawing.Point(456, 178);
+            this.centerRight_lbl.Name = "centerRight_lbl";
+            this.centerRight_lbl.Size = new System.Drawing.Size(164, 113);
+            this.centerRight_lbl.TabIndex = 5;
+            this.centerRight_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.centerRight_lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // bottomLeft_lbl
+            // 
+            this.bottomLeft_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bottomLeft_lbl.Location = new System.Drawing.Point(49, 329);
+            this.bottomLeft_lbl.Name = "bottomLeft_lbl";
+            this.bottomLeft_lbl.Size = new System.Drawing.Size(167, 108);
+            this.bottomLeft_lbl.TabIndex = 6;
+            this.bottomLeft_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bottomLeft_lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // bottomCenter_lbl
+            // 
+            this.bottomCenter_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bottomCenter_lbl.Location = new System.Drawing.Point(249, 329);
+            this.bottomCenter_lbl.Name = "bottomCenter_lbl";
+            this.bottomCenter_lbl.Size = new System.Drawing.Size(172, 108);
+            this.bottomCenter_lbl.TabIndex = 7;
+            this.bottomCenter_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bottomCenter_lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // bottomRight_lbl
+            // 
+            this.bottomRight_lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bottomRight_lbl.Location = new System.Drawing.Point(456, 329);
+            this.bottomRight_lbl.Name = "bottomRight_lbl";
+            this.bottomRight_lbl.Size = new System.Drawing.Size(164, 108);
+            this.bottomRight_lbl.TabIndex = 8;
+            this.bottomRight_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bottomRight_lbl.Click += new System.EventHandler(this.label_click);
+            // 
+            // declareWinner_lbl
+            // 
+            this.declareWinner_lbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.declareWinner_lbl.Location = new System.Drawing.Point(49, 463);
+            this.declareWinner_lbl.Name = "declareWinner_lbl";
+            this.declareWinner_lbl.Size = new System.Drawing.Size(571, 44);
+            this.declareWinner_lbl.TabIndex = 9;
+            this.declareWinner_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // newGame_btn
+            // 
+            this.newGame_btn.Location = new System.Drawing.Point(117, 531);
+            this.newGame_btn.Name = "newGame_btn";
+            this.newGame_btn.Size = new System.Drawing.Size(156, 43);
+            this.newGame_btn.TabIndex = 10;
+            this.newGame_btn.Text = "New Game";
+            this.newGame_btn.UseVisualStyleBackColor = true;
+            this.newGame_btn.Click += new System.EventHandler(this.newGame_btn_Click);
+            // 
+            // exitGame_btn
+            // 
+            this.exitGame_btn.Location = new System.Drawing.Point(386, 531);
+            this.exitGame_btn.Name = "exitGame_btn";
+            this.exitGame_btn.Size = new System.Drawing.Size(169, 43);
+            this.exitGame_btn.TabIndex = 11;
+            this.exitGame_btn.Text = "Exit";
+            this.exitGame_btn.UseVisualStyleBackColor = true;
+            this.exitGame_btn.Click += new System.EventHandler(this.exitGame_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 602);
+            this.Controls.Add(this.exitGame_btn);
+            this.Controls.Add(this.newGame_btn);
+            this.Controls.Add(this.declareWinner_lbl);
+            this.Controls.Add(this.bottomRight_lbl);
+            this.Controls.Add(this.bottomCenter_lbl);
+            this.Controls.Add(this.bottomLeft_lbl);
+            this.Controls.Add(this.centerRight_lbl);
+            this.Controls.Add(this.center_lbl);
+            this.Controls.Add(this.centerLeft_lbl);
+            this.Controls.Add(this.topRight_lbl);
+            this.Controls.Add(this.topCenter_lbl);
+            this.Controls.Add(this.topLeft_lbl);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label topLeft_lbl;
+        private System.Windows.Forms.Label topCenter_lbl;
+        private System.Windows.Forms.Label topRight_lbl;
+        private System.Windows.Forms.Label centerLeft_lbl;
+        private System.Windows.Forms.Label center_lbl;
+        private System.Windows.Forms.Label centerRight_lbl;
+        private System.Windows.Forms.Label bottomLeft_lbl;
+        private System.Windows.Forms.Label bottomCenter_lbl;
+        private System.Windows.Forms.Label bottomRight_lbl;
+        private System.Windows.Forms.Label declareWinner_lbl;
+        private System.Windows.Forms.Button newGame_btn;
+        private System.Windows.Forms.Button exitGame_btn;
+    }
+}
+
---
+++ CST-150Activity13/CST-150Activity13/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150Activity13
+{
+    public partial class Form1 : Form
+    {
+        //true = X turn; false = Y turn;
+        bool turn = true;
+        int turn_count = 0;
+        int [,] labelGrid =
+        {
+            {0,0,0},
+            {0,0,0},
+            {0,0,0}
+        };
+
+        int posX;
+        int posY;         //left off 0120 on 20211101
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitGame_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label_click(object sender, EventArgs e)
+        {
+            Label l = (Label)sender;
+
+            //Map buttons to positions on game grid.
+            if (l == topLeft_lbl)
+            {
+                posY = 0;
+                posX = 0;
+
+                playerValueOnGrid(posY, posX);
+            }
+            else if (l == topCenter_lbl)
+            {
+                posY = 0;
+                posX = 1;
+
+                playerValueOnGrid(posY, posX);
+            }
+            else if (l == topRight_lbl)
+            {
+                posY = 0;
+                posX = 2;
+
+                playerValueOnGrid(posY, posX);
+            }
+            else if (l == centerLeft_lbl)
+            {
+                posY = 1;
+                posX = 0;
+
+                playerValueOnGrid(posY, posX);
+            }
+            else if (l == center_lbl)
+            {
+                posY = 1;
+                posX = 1;
+
+                playerValueOnGrid(posY, posX);
+            }
+            else if (l == centerRight_lbl)
+            {
+                posY = 1;
+                posX = 2;
+
+                playerValueOnGrid(posY, posX);
+            }
+            else if (l == bottomLeft_lbl)
+            {
+                posY = 2;
+                posX = 0;
+
+                playerValueOnGrid(posY, posX);
+            }
+            else if (l == bottomCenter_lbl)
+            {
+                posY = 2;
+                posX = 1;
+
+                playerValueOnGrid(posY, posX);
+            }
+            else if (l == bottomRight_lbl)
+            {
+                posY = 2;
+                posX = 2;
+
+                playerValueOnGrid(posY, posX);
+            }
+
+            if (turn == true)
+            {
+                l.Text = "X";
+         
+            }
+            else
+            {
+                l.Text = "O";
+            }
+
+            turn = !turn;
+            l.Enabled = false;
+            turn_count++;
+            checkForWinner();
+        }
+
+        private void checkForWinner()
+        {
+            bool winnerExists = false;
+
+
+            // horizontal winning conditions
+            if (labelGrid[0, 0] == 1 && labelGrid[0, 1] == 1 && labelGrid[0, 2] == 1 || labelGrid[0, 0] == 2 && labelGrid[0, 1] == 2 && labelGrid[0, 2] == 2) 
+                winnerExists = true;
+            else if (labelGrid[1, 0] == 1 && labelGrid[1, 1] == 1 && labelGrid[1, 2] == 1 || labelGrid[1, 0] == 2 && labelGrid[1, 1] == 2 && labelGrid[1, 2] == 2)
+                winnerExists = true;
+            else if (labelGrid[2, 0] == 1 && labelGrid[2, 1] == 1 && labelGrid[2, 2] == 1 || labelGrid[2, 0] == 2 && labelGrid[2, 1] == 2 && labelGrid[2, 2] == 2)
+                winnerExists = true;
+            //vertical winning conditions
+           else if (labelGrid[0, 0] == 1 && labelGrid[1, 0] == 1 && labelGrid[2, 0] == 1 || labelGrid[0, 0] == 2 && labelGrid[1, 0] == 2 && labelGrid[2, 0] == 2)
+                winnerExists = true;
+            else if (labelGrid[0, 1] == 1 && labelGrid[1, 1] == 1 && labelGrid[2, 1] == 1 || labelGrid[0, 1] == 2 && labelGrid[1, 1] == 2 && labelGrid[2, 1] == 2)
+                winnerExists = true;
+            else if (labelGrid[0, 2] == 1 && labelGrid[1, 2] == 1 && labelGrid[2, 2] == 1 || labelGrid[0, 2] == 2 && labelGrid[1, 2] == 2 && labelGrid[2, 2] == 2)
+                winnerExists = true;
+            //diagonal winning conditions
+            else if (labelGrid[0, 0] == 1 && labelGrid[1, 1] == 1 && labelGrid[2, 2] == 1 || labelGrid[0, 0] == 2 && labelGrid[1, 1] == 2 && labelGrid[2, 2] == 2)
+                winnerExists = true;
+            else if (labelGrid[0, 2] == 1 && labelGrid[1, 1] == 1 && labelGrid[2, 0] == 1 || labelGrid[0, 2] == 2 && labelGrid[1, 1] == 2 && labelGrid[2, 0] == 2)
+                winnerExists = true;
+            /**
+            //horizontal winning conditions
+            if ((topLeft_lbl.Text == topCenter_lbl.Text) && (topCenter_lbl.Text == topRight_lbl.Text) && (!topLeft_lbl.Enabled))
+                winnerExists = true;
+           else if ((centerLeft_lbl.Text == center_lbl.Text) && (center_lbl.Text == centerRight_lbl.Text) && (!centerLeft_lbl.Enabled))
+                winnerExists = true;
+           else if ((bottomLeft_lbl.Text == bottomCenter_lbl.Text) && (bottomCenter_lbl.Text == bottomRight_lbl.Text) && (!bottomLeft_lbl.Enabled))
+                winnerExists = true;
+            //vertical winning conditions
+           else if ((topLeft_lbl.Text == centerLeft_lbl.Text) && (centerLeft_lbl.Text == bottomLeft_lbl.Text) && (!topLeft_lbl.Enabled))
+                winnerExists = true;
+           else if ((topCenter_lbl.Text == center_lbl.Text) && (center_lbl.Text == bottomCenter_lbl.Text) && (!topCenter_lbl.Enabled))
+                winnerExists = true;
+           else if ((topRight_lbl.Text == centerRight_lbl.Text) && (centerRight_lbl.Text == bottomRight_lbl.Text) && (!topRight_lbl.Enabled))
+                winnerExists = true;
+            //diagonal winning conditions
+           else if ((topLeft_lbl.Text == center_lbl.Text) && (center_lbl.Text == bottomRight_lbl.Text) && (!topLeft_lbl.Enabled))
+                winnerExists = true;
+           else if ((topRight_lbl.Text == center_lbl.Text) && (center_lbl.Text == bottomLeft_lbl.Text) && (!bottomLeft_lbl.Enabled))
+                winnerExists = true;
+            */
+
+
+            //shows the winner of the game in the lower label box of the form
+            if (winnerExists)
+            {
+                disableButtons();
+
+                String winner = "";
+                if(turn == true)
+                {
+                    winner = "O";
+                }
+                else
+                {
+                    winner = "X";
+                }
+
+                declareWinner_lbl.Text = "Player " + "'" + winner + "' " + "is the winner!";
+            }
+            else
+            {
+                if (turn_count == 9)
+                {
+                    declareWinner_lbl.Text = "Looks like there is no winner. It was a draw!";
+                }
+            }
+        }
+
+        private void disableButtons()
+        {
+            try
+            {
+
+
+                foreach (Label c in Controls)
+                {
+                    
+                    Label l = (Label)c;
+                    l.Enabled = false;
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void newGame_btn_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+           
+
+        }
+
+        //marks grid with player value where players make moves
+        private void playerValueOnGrid(int posY, int posX)
+        {
+            //if X turn 
+            if(turn == true)
+            {
+                //change value on the game grid (2dArray) with 1 signifying player 'X'
+                labelGrid[posY, posX] = 1;
+                //connects the player value to the label on the game
+                String labelPosition = "The position of the X is [" + posY + ", " + posX + "]";
+                Console.WriteLine(labelPosition);
+
+            }
+            else
+            {
+                //On O's turn place the player value in the 2dArray with 2 signifying player 'O'
+                if (turn == false)
+                {
+                    labelGrid[posY, posX] = 2;
+                    //connects the player value to the label on the game
+                    String labelPosition = "The position of the O is [" + posY + ", " + posX + "]";
+                    Console.WriteLine(labelPosition);
+                }
+            }
+        }
+    }
+}
